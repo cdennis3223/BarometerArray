91c9ad8b204ffe49226aa21b77bfbd9bc2d94523
SAM <kolob257@gmail.com>
2026-04-08 15:38:49 -0700

    Updated the platformio.ini file and  fixed BatMGT.h error

--- firmware/include/BatMGMT.h
+++ firmware/include/BatMGMT.h
@@ -3,12 +3,6 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 
-<<<<<<< HEAD
 #define I2C_MASTER_SCL 41
 #define I2C_MASTER_SDA 42
 #define MAX_ALERT 40
-=======
-#define I2C_MASTER_SCL 21
-#define I2C_MASTER_SDA 35
-#define MAX_ALERT 36
->>>>>>> c7c3d53a332d960fc8052e29a8338439a4376229
