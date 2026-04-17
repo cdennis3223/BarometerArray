322624053023cfb5bd799afc64185783b5126c4f
cdennis3223 <carldennis3223@gmail.com>
2026-04-17 12:42:56 -0700

    Fixed BatMGMT.h

--- firmware/include/BatMGMT.h
+++ firmware/include/BatMGMT.h
@@ -6,6 +6,8 @@
 #define I2C_MASTER_SCL 41
 #define I2C_MASTER_SDA 42
 #define MAX_ALERT 40
+#define I2C_NUM I2C_NUM_0
+#define BATMGMT_ADDR 0x36
 
 void BatMGMT_init(void);
 float BatMGMT_readVoltage(void);
