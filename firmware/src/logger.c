b8005efa06a1c7c9de0d4848b9ee086c2139b0ca
cdennis3223 <carldennis3223@gmail.com>
2026-04-17 12:47:48 -0700

    logger fix

--- firmware/src/logger.c
+++ firmware/src/logger.c
@@ -6,7 +6,7 @@ void logger_task(void *args) {
     while (1) {
         sample_t s;
         if (ring_buffer_pop(rb, &s)) {
-            sd_log_sample(s.pressure, s.temperature, s.timestamp_ms, s.Voltage, s.soc);
+            sd_log_sample(s.pressure, s.temperature, s.timestamp_ms, s.Voltage, s.SOC, s.seq);
         } else {
             vTaskDelay(pdMS_TO_TICKS(100)); // Sleep briefly if no data
         }
